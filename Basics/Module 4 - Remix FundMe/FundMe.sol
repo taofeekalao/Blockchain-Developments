abc1053e1ba6d060fea74a6b9afc1e864e9edb43
Taofeek Alao <taofeekalao@gmail.com>
2022-08-14 07:30:41 +0200

    Completed withdraw function
    Completed withdraw function with modifier "owner" added to the contract

--- Basics/Module 4 - Remix FundMe/FundMe.sol
+++ Basics/Module 4 - Remix FundMe/FundMe.sol
@@ -11,8 +11,13 @@ contract FundMe {
     uint256 public minimumUSD = 50 * 1e18; // 1 * 10 ** 18
 
     address[] public funders;
-
     mapping(address => uint256) public addressToAmountFunders;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         // Checking if what was send is sufficient for transaction
@@ -21,21 +26,28 @@ contract FundMe {
         addressToAmountFunders[msg.sender] = msg.value;
     }
 
-    function withdrawal() public {
+    function withdrawal() public onlyOwner {
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunders[funder] = 0;
         }
+        // Reset the array after complete withdrawal
+        funders = new address[](0);
 
         // transfer
-        payable(msg.sender).transfer(address(this).balance);
-        // send
-        bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        require(sendSuccess, "Send failed!");
-        // call
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed!");
+        // // call
         (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed!");
         
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Sender is not the owner!");
+        _;
     }
 
 }
