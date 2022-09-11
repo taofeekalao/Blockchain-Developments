76e15e66f7c766ef776633d6271cc0ae2528fccb
Taofeek Alao <taofeekalao@gmail.com>
2022-09-11 08:19:06 +0200

    Initiated FundMe Hardhat
    Initiated FundMe Hardhat
    Completed FundMe Hardhat
    Connected FundMe Hardhat to Testnets
    Created Tests for FundMe Hardhat
    Refactored FundMe Hardhat
    Performed Gas Optimisation on FundMe Hardhat

---
+++ Basics/Module 7 - Hadhat FundMe/contracts/FundMe.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+
+error FundMe__NotOwner();
+
+/** @title A contract for crowd funding
+ *  @author Taofeek Alao
+ *  @notice This contract is to demo a sample funding contract
+ *  @dev This implements price feed as our library
+ */
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 50 * 1e18; // 1 * 10 ** 18
+    address[] public s_funders;
+    mapping(address => uint256) public s_addressToAmountFunded;
+    address public immutable i_owner;
+    AggregatorV3Interface public s_priceFeed;
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
+
+    constructor(address priceFeedAddress) {
+        i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    /**
+     * @notice Introducing Fallback & Receive Functions In Case Someone.
+     * @notice Calls Our Contract Without Specifying Function Correctly.
+     */
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    function fund() public payable {
+        /// @notice Checking if what was sent is sufficient for transaction
+        require(
+            msg.value.getConversionRate(s_priceFeed) > MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
+        s_funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdrawal() public payable onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < s_funders.length;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        /// @notice Reset the array after complete withdrawal
+        s_funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed!");
+    }
+
+    function cheaperWithdrawal() public payable onlyOwner {
+        address[] memory funders = s_funders;
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        require(success, "Call failed");
+    }
+}
---
+++ Basics/Module 7 - Hadhat FundMe/deploy/01-deploy-fund-me.js
@@ -0,0 +1,50 @@
+const { networks, log } = require("../hardhat.config");
+
+// module.exports.default = deployFunc;
+
+// const { getNamedAccounts, deployments } = require("hardhat");
+
+// // module.exports = async (hre) => {
+// //   const { getNamedAccounts, deployments } = hre;
+// // };
+
+const {
+  networkConfig,
+  developmentChains,
+} = require("../helper-hardhat-config");
+const { network } = require("hardhat");
+const { verify } = require("../utils/verify");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+
+  let ethUsdPriceFeedAddress;
+  if (developmentChains.includes(network.name)) {
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+    ethUsdPriceFeedAddress = ethUsdAggregator.address;
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  }
+
+  const args = [ethUsdPriceFeedAddress];
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: args, // put price feed address
+    log: true,
+    waitConfirmation: network.config.blockConfirmation || 5,
+  });
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(fundMe.address, args);
+  }
+  log("---------------------------------------------------");
+};
+
+module.exports.tags = ["all", "fundme"];
