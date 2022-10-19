d4d2a1f1a838fc0413e776813694975ac53c355e
Taofeek Alao <taofeekalao@gmail.com>
2022-10-19 22:10:19 +0200

    Almost done with module 9 with errors on vrfCoordinatorV2

---
+++ Basics/Module 9 - Hardhat Smart Contract Lottery/contracts/test/VRFCoordinatorV2Mock.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
---
+++ Basics/Module 9 - Hardhat Smart Contract Lottery/helper-hardhat-config.js
@@ -0,0 +1,39 @@
+const { ethers } = require("hardhat")
+
+const networkConfig = {
+    4: {
+        name: "rinkeby",
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E*57ab",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab2413cea0a43ea20a6049a85cc807cc",
+        subscriptionId: "2",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+
+    5: {
+        name: "goerli",
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x708701a1DfF4f478de54383E49a627eD4852C816",
+        subscriptionId: "5",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+
+    31337: {
+        name: "localhost",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        subscriptionId: "588",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
